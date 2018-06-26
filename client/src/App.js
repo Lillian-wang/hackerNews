1454b038a2670d0be6c6354df69c02d2cc585efd
Lillian Wang <lillian.wang@collinear.com>
2018-06-25 20:08:37 -0700

    Change the time stamp

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
   componentDidMount() {
     this.getLists()
       .then(res => {
-        console.log('API res', res)
         this.setState({ lists: res })
       })
       .catch(err => console.log(err));
@@ -22,50 +21,22 @@ class App extends Component {
     return body;
   };
 
-
-  addItem(itemContent) {
-    console.log('itemContent', itemContent)
-    fetch('/api/addList', {
-      method: 'POST',
-      body: JSON.stringify({
-        itemContent
-      }),
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
-    }).then(() => {
-      this.getLists().then(res => {
-        console.log('res', res)
-
-      })
-        .catch(err => console.log(err));
-    })
-  }
-
-  showNews(itemId) {
-    const response = fetch(`https://hacker-news.firebaseio.com/v0/item/${itemId}.json?print=pretty`).then(function (response) {
-      return response.json();
-    })
-      .then(function (myJson) {
-        console.log(myJson);
-      });
-  }
-
-
   render() {
     return (
       <div className="App">
         <div className="header">
-          <span className="title"><img src="https://news.ycombinator.com/y18.gif" />Hacker News</span>
+          <span className="title"><img alt="icon" src="https://news.ycombinator.com/y18.gif" />Hacker News</span>
           <span>new | comments | show | ask | jobs | submit </span>
+          <span className="login">Login</span>
         </div>
         <ul className="list-item-container">
-          {this.state.lists.map((list, index) => <ListItem index={index} listItem={list} key={index} onClick={this.showNews.bind(this)} />)}
+          {this.state.lists.map((list, index) => <ListItem index={index} listItem={list} key={index} />)}
         </ul>
       </div>
     );
   }
 }
+
 class ListItem extends Component {
   state = {};
   componentDidMount() {
@@ -85,12 +56,12 @@ class ListItem extends Component {
 
   render() {
     return (
-      <div >
+      <div>
         <li className="list-item" key={this.props.index}>
           {this.state.type === 'story' &&
             <div>
-              <span className="upper-arrow"></span><a className="article-title" href="this.state.url"> {this.state.title}</a>
-              <p className="sub-title"> {this.state.score} points by {this.state.by}  {Math.round((new Date().getTime() - this.state.time) / 60000 * 60 * 24)} days ago | past | web | {this.state.descendants} comments </p>
+              <span className="upper-arrow"></span><a className="article-title" href={this.state.url}> {this.state.title}</a>
+              <p className="sub-title"> {this.state.score} points by {this.state.by} {Math.round((new Date().getTime() - this.state.time * 1000) / (60000 * 60 * 24 * 365))} years ago | past | web | {this.state.descendants} comments </p>
             </div>
           }
         </li>
